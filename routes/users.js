baa08bd6f77ac220f1ab013637706f59579d055a
Andrey <dreususJ@yandex.ru>
2023-03-24 21:14:17 +0300

    No newline at en of file

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const {
   updateUserAvatar
 } = require('../controllers/users')
 
-router.get('/users', getUsers) 
+router.get('/users', getUsers)
 router.get('/users/:id', getUserById)
 router.post('/users', createUser)
 router.patch('/users/me', updateProfile)
