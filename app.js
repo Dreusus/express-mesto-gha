1906786920c634e52ff2aaa2ac7a5aec2cb8f5e8
Andrey <dreususJ@yandex.ru>
2023-03-28 22:56:12 +0300

    how test autotest

--- app.js
+++ app.js
@@ -25,12 +25,12 @@ app.post(
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
-      avatar: Joi.string().regex(/^(https?:\/\/)?(www\.)?[a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]*#?/),
+      avatar: Joi.string().regex(/^(https?:\/\/)([\da-z\.-]+)\.([a-z\.]{2,6})([\/\#\w \.-]*)*\/?$/),
       email: Joi.string().required().email(),
       password: Joi.string().required(),
     }),
   }),
-  login,
+  createUser,
 );
 
 app.post(
@@ -41,7 +41,7 @@ app.post(
       password: Joi.string().required(),
     }),
   }),
-  createUser,
+  login,
 );
 app.use(auth);
 app.use('/', userRouter);
